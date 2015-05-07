e4b6f51b809e252f0afe00e8596955a5ae996943
jgallagheriii <jgallagheriii@yahoo.com>
2015-05-07 16:12:43 -0400

    fist commit

---
+++ src/com/example/digitalclock/ClockFragment.java
@@ -0,0 +1,7 @@
+package com.example.digitalclock;
+
+import android.app.Fragment;
+
+public class ClockFragment extends Fragment {
+
+}
---
+++ src/com/example/digitalclock/ClockView.java
@@ -0,0 +1,19 @@
+package com.example.digitalclock;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class ClockView extends SurfaceView {
+
+	public ClockView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClockView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
